ab8dee74d084f9ca17c9846bdd8edd41999c28f9
Sergio Chamorro <sergiochamo96@gmail.com>
2023-02-04 18:48:04 -0300

    get list of repositories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import github from "./db";
+import { useEffect, useState, useCallback } from "react";
+import query from "./Query";
+
 function App() {
+  const [userName, setUserName] = useState("");
+  const [repoList, setRepoList] = useState(null);
+
+  const fetchData = useCallback(() => {
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query),
+    })
+    .then(response => response.json())
+    .then(data => {
+      const viewer = data.data.viewer;
+      setUserName(viewer.name);
+      setRepoList(viewer.repositories.nodes);
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <p>Hey there {userName}</p>
+
+      {
+        repoList && (
+          <ul className="list-group list-group-flush">
+            {
+              repoList.map(repo => (
+                <li className="list-group-item" key={repo.id.toString()}>
+                  <a className="h5 mb-0 text-decoration-none" href={repo.url}>
+                    {repo.name}
+                  </a>
+                  <p className="small">{repo.description}</p>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
     </div>
   );
 }
