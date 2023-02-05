5733d1e271bafea9570a11cc65873b2d9144414b
Sergio Chamorro <sergiochamo96@gmail.com>
2023-02-05 11:49:37 -0300

    change query to dynamic and add search component

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import github from "./db";
 import { useEffect, useState, useCallback } from "react";
 import query from "./Query";
 import RepoInfo from "./RepoInfo";
+import SearchBox from "./SearchBox";
 
 function App() {
   const [userName, setUserName] = useState("");
   const [repoList, setRepoList] = useState(null);
+  const [pageCount, setPageCount] = useState(10);
+  const [queryString, setQueryString] = useState("");
+  const [totalCount, setTotalCount] = useState(null);
 
   const fetchData = useCallback(() => {
+    const queryText = JSON.stringify(query(pageCount, queryString));
+
     fetch(github.baseURL, {
       method: "POST",
       headers: github.headers,
-      body: JSON.stringify(query),
+      body: queryText,
     })
     .then(response => response.json())
     .then(data => {
       const viewer = data.data.viewer;
       const repos = data.data.search.nodes;
+      const total = data.data.search.repositoryCount;
       setUserName(viewer.name);
       setRepoList(repos);
+      setTotalCount(total);
       console.log(data);
     })
     .catch(err => {
       console.log(err);
     });
-  }, []);
+  }, [pageCount, queryString]);
 
   useEffect(() => {
     fetchData();
@@ -36,7 +44,13 @@ function App() {
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
       <p>Hey there {userName}</p>
-
+      <SearchBox 
+        totalCount={totalCount}
+        pageCount={pageCount}
+        queryString={queryString}
+        onTotalChange={(myNumber) => {setPageCount(myNumber)}}
+        onQueryChange={(myString) => {setQueryString(myString)}}
+      />
       {
         repoList && (
           <ul className="list-group list-group-flush">
